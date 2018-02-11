1043da724dec5155c7e91dcaf5b4a70e6452eaa8
glasskz14 <rkaew@outlook.co.th>
2018-02-12 01:23:22 +0700

    Initial commit

---
+++ Register.php
@@ -0,0 +1,130 @@
+
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Register</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="inner">
+
+							<!-- Logo -->
+								<a href="index.html" class="logo">
+									<span class="symbol"><img src="images/11.svg" alt="" /></span><span class="title">LOST And FOUND</span>
+								</a>
+
+							<!-- Nav -->
+								<nav>
+									<ul>
+										<li><a href="#menu">Menu</a></li>
+									</ul>
+								</nav>
+
+						</div>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<h2>Menu</h2>
+						<ul>
+							<li><a href="index.html">หน้าเเรก (Home)</a></li>
+							<li><a href="Profile.html">ข้อมูลส่วนตัว (Profile)</a></li>
+							<li><a href="FromLost.html">เเจ้งทำหาย (Lost)</a></li>
+							<li><a href="FromFound.html">เเจ้งเจอของ (Found)</a></li>
+							<li><a href="Register.html">สมัครสมาชิก (Register)</a></li>
+							<li><a href="Login.html">เข้าสู่ระบบ (Login)</a></li>
+							<li><a href="About.html">เกี่ยวกับ (About)</a></li>
+						</ul>
+					</nav>
+
+
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+							<h1>Register</h1>
+							<section>
+								<title>Register ลงสมัครสมาชิก</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+
+<div class="w3-card-4">
+  <div class="w3-container w3-blue">
+    <h2>สมัครสมาชิก</h2>
+  </div>
+  <?php include('error.php'); ?>
+  <form class="w3-container" action="/action_page.php">
+    <p>      
+    <p>
+    <label class="w3-text-brown"><b>อีเมลย์</b></label>
+    <input class="w3-input w3-border w3-sand" name="email" type="text"></p>
+    <p>
+    <p>
+    <label class="w3-text-brown"><b>รหัสผ่าน</b></label>
+    <input class="w3-input w3-border w3-sand" name="password_1" type="Password"></p>
+    <p>
+     <p>
+    <label class="w3-text-brown"><b>รหัสผ่านอีกครั้ง</b></label>
+    <input class="w3-input w3-border w3-sand" name="assword_2" type="Password"></p>
+    <p>
+    <p>
+    <label class="w3-text-brown"><b>ชื่อ</b></label>
+    <input class="w3-input w3-border w3-sand" name="fname" type="text"></p>
+    <p>
+    <p>
+    <label class="w3-text-brown"><b>นามสกุล</b></label>
+    <input class="w3-input w3-border w3-sand" name="lname" type="text"></p>
+    <p>
+    <p>
+    <label class="w3-text-brown"><b>เบอร์โทรศัพย์</b></label>
+    <input class="w3-input w3-border w3-sand" name="tel" type="num"></p>
+    <p>
+    	<center>
+    <a href="index.html" class="w3-btn w3-blue">ยกเลิก</button></a> 
+    <a href="Forget.html" class="w3-btn w3-blue">สมัครสมาชิก</button></a></p>   
+</p>
+</center>
+  </form>
+</div>
+
+</body>
+
+								</section>
+							<section>
+								<h2>Follow</h2>
+								<ul class="icons">
+									<li><a href="#" class="icon style2 fa-twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="#" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
+									<li><a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
+									<li><a href="#" class="icon style2 fa-dribbble"><span class="label">Dribbble</span></a></li>
+									<li><a href="#" class="icon style2 fa-github"><span class="label">GitHub</span></a></li>
+									<li><a href="#" class="icon style2 fa-500px"><span class="label">500px</span></a></li>
+									<li><a href="#" class="icon style2 fa-phone"><span class="label">Phone</span></a></li>
+									<li><a href="#" class="icon style2 fa-envelope-o"><span class="label">Email</span></a></li>
+								</ul>
+						
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
